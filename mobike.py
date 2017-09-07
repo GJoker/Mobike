675c312cf5573b2cbbdddfc890c087ccb6d19944
Rui Gao <raygao@epoque.shoes>
2017-09-07 17:57:30 +0800

    update test

--- mobike.py
+++ mobike.py
@@ -11,30 +11,64 @@ train_path = 'data/train.csv'
 test_path = 'data/test.csv'
 cache_path = 'cache_1/'
 
-def get_sample(train):
-  result_path = cache_path + 'sample_%d.hdf' % (train.shape[0] * test.shape[0])
+
+def get_sloc_to_eloc(train, test):
+  result_path = cache_path + 'sloc_to_eloc.hdf'
   if os.path.exists(result_path):
-    result = pd.read_hdf(result_path, 'w')
+    result = pd.read_hdf(result_path, 'r')
   else:
-    user_eloc = train[['userid', 'geohashed_end_loc']].drop_duplicates()
-    user_eloc.rename(columns={'geohashed_end_loc': 'label'}, inplace=True)
-    result = pd.merge(train, user_eloc, on='userid', how='left')
-    sloc_to_eloc = train[['geohashed_start_loc', 'geohashed_end_loc']].drop_duplicates()
-    sloc_to_eloc.rename(columns={'geohashed_end_loc': 'label'}, inplace=True)
-    result = pd.merge(result, sloc_to_eloc, on='geohashed_start_loc', how='left')
+    sloc_to_eloc = train[['geohashed_start_loc', 'geohashed_end_loc']].sort_values('geohashed_start_loc')
+    sloc_to_eloc = train.groupby(['geohashed_start_loc', 'geohashed_end_loc'], as_index=False)[
+      'geohashed_start_loc'].agg({'sloc_eloc_count': 'count'})
+    sloc_to_eloc.sort_values('sloc_eloc_count', inplace=True)
+    result = sloc_to_eloc.groupby('geohashed_start_loc').tail(3)
+    result.rename(columns={'geohashed_end_loc': 'label'}, inplace=True)
+    result.to_hdf(result_path, 'w', complib='blosc', complevel=5)
   return result
 
 
-def make_train_set(train):
-  result = get_sample(train)
+def get_user_to_loc(train, test):
+  result_path = cache_path + 'user_to_loc.hdf'
+  if os.path.exists(result_path):
+    result = pd.read_hdf(result_path, 'r')
+  else:
+    user_to_sloc = train[['userid', 'geohashed_start_loc']].drop_duplicates()
+    user_to_sloc.rename(columns={'geohashed_start_loc': 'label'}, inplace=True)
+    user_to_eloc = train[['userid', 'geohashed_end_loc']].drop_duplicates()
+    user_to_eloc.rename(columns={'geohashed_end_loc': 'label'}, inplace=True)
+    result = pd.concat([user_to_sloc, user_to_eloc]).drop_duplicates()
+    result = pd.merge(test, result, how='left', on='userid')
+    result.to_hdf(result_path, 'w', complib='blosc', complevel=5)
   return result
+
+
+def get_sample(train, test):
+  result_path = cache_path + 'sample.hdf'
+  if os.path.exists(result_path):
+    result = pd.read_hdf(result_path, 'r')
+  else:
+    user_to_loc = get_user_to_loc(train, test)
+    sloc_to_eloc = get_sloc_to_eloc(train, test)
+    result = pd.merge(train, user_to_loc, on='userid', how='left')
+    result = pd.merge(result, sloc_to_eloc, on='geohashed_start_loc', how='left').drop_duplicates()
+    result.to_hdf(result_path, 'w', complib='blosc', complevel=5)
+  return result
+
+
+def make_train_set(train, test):
+  result = get_sample(train, test)
+  return result
+
 
 if __name__ == '__main__':
   t0 = time.time()
   train = pd.read_csv(train_path).drop('biketype', axis=1)
   test = pd.read_csv(test_path).drop('biketype', axis=1)
 
-  train_set = make_train_set(train)
-  print train_set.shape()
+  train1 = train[(train['starttime'] < '2017-05-21 00:00:00')]
+  train2 = train[(train['starttime'] >= '2017-05-21 00:00:00')]
 
-                          
+  train_set = make_train_set(train1, train2)
+
+  # train1 = train[()]
+  # train_set = make_train_set(train)
--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import numpy as np
 import Geohash
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import json
+import datetime
 
 # date = pd.date_range('20170822', periods=7)
 # print date
@@ -22,9 +24,9 @@ import statsmodels.api as sm
 # print s2
 
 #  orderid  userid  bikeid  biketype  starttime  geohashed_start_loc  geohashed_end_loc
-#
-train = pd.read_csv('./data/train.csv')
-test = pd.read_csv('./data/test.csv')
+
+# train = pd.read_csv('./data/train.csv')
+# test = pd.read_csv('./data/test.csv')
 
 # locs = [loc[0:3] for loc in train['geohashed_start_loc'].values]
 # for loc in set(locs):
@@ -75,8 +77,8 @@ test = pd.read_csv('./data/test.csv')
 # a = a.groupby('key1').tail(2)
 # print a
 
-# train = pd.read_csv('./data/train.csv')
-# # print type(train['starttime'].values)
+train = pd.read_csv('./data/train.csv')
+# print type(train['starttime'].values)
 # # train['starttime'] = pd.to_datetime(train['starttime']).dt.date
 # # print train
 #
@@ -84,19 +86,21 @@ test = pd.read_csv('./data/test.csv')
 # train1 = train[(train['starttime'] < '2017-05-23 00:00:00')]
 # train2 = train[(train['starttime'] >= '2017-05-23 00:00:00')]
 # #
-user_count = train.groupby('userid', as_index=False)['geohashed_start_loc'].agg({'user_count': 'count'})
+# user_count = train.groupby('userid', as_index=False)['geohashed_start_loc'].agg({'user_count': 'count'})
 # print train[(train['userid'] == 2730)].groupby('geohashed_start_loc', as_index=False)['geohashed_start_loc'].agg({'number': 'count'})
 # test_csv = train[(train['userid'] == 2730)].groupby(['geohashed_start_loc', 'geohashed_end_loc'], as_index=False)['geohashed_end_loc'].agg({'number': 'count'})
 # test_csv.to_csv('test.csv')
-count = user_count.groupby('user_count', as_index=False)['user_count'].agg({'count': 'count'})
-insertRow = pd.DataFrame([[0, 0]], columns=['user_count', 'count'])
-count = pd.concat([insertRow, count], ignore_index=True)
-sumAll = count['count'].sum()
-result = count['count']/sumAll
-print result
-result = result.cumsum()
-result.plot()
-plt.show()
+# sloc_to_eloc = train.groupby(['geohashed_start_loc', 'geohashed_end_loc'], as_index=False)[
+#       'geohashed_start_loc'].agg({'sloc_eloc_count': 'count'})
+# # count = user_count.groupby('user_count', as_index=False)['user_count'].agg({'count': 'count'})
+# insertRow = pd.DataFrame([[0, 0]], columns=['user_count', 'count'])
+# count = pd.concat([insertRow, count], ignore_index=True)
+# sumAll = count['count'].sum()
+# result = count['count']/sumAll
+# print result
+# result = result.cumsum()
+# result.plot()
+# plt.show()
 # sumAll = count['count'].sum()
 #
 # xaxis = list(count['user_count'])
@@ -109,9 +113,10 @@ plt.show()
 
 # sloc_eloc_count = train.groupby(['geohashed_start_loc', 'geohashed_end_loc'], as_index=False)[
 #       'geohashed_end_loc'].agg({'sloc_eloc_count': 'count'}) # 统计各个起始-终止区域个数
-# sloc_eloc_count.sort_values('sloc_eloc_count', inplace=True)
+# sloc_eloc_count.sort_values(by='sloc_eloc_count', inplace=True)
 # print sloc_eloc_count
-# sloc_eloc_count = sloc_eloc_count.groupby('geohashed_start_loc').tail(3) # 统计各个起始点去的最多的3个终点
+# sloc_eloc_count = sloc_eloc_count[(sloc_eloc_count['geohashed_start_loc'] == 'wx4fftr')]# 统计各个起始点去的最多的3个终点
+# print sloc_eloc_count
 # result = pd.merge(train2[['orderid', 'geohashed_start_loc']], sloc_eloc_count, on='geohashed_start_loc', how='left')
 # result = result[['orderid', 'geohashed_end_loc']]
 #
@@ -127,3 +132,39 @@ plt.show()
 # 安徽合肥 云南红河 云南玉溪 浙江杭州 北京 湖南株洲 四川成都 四川资阳 福建福州 福建宁德
 # for loc in locs:
 #   print [round(i, 4) for i in Geohash.decode_exactly(loc)[0:2]]
+
+# sloc_to_eloc = train[['geohashed_start_loc', 'geohashed_end_loc']].sort_values('geohashed_start_loc')
+train1 = train
+train1['starttime'] = pd.to_datetime(train1['starttime'])
+train1['weekday'] = train1['starttime'].apply(lambda x: x.weekday())
+train1['time'] = train1['starttime'].apply(lambda x: x.time())
+
+train1['weekday'][(train1['weekday'] <= 3)] = 0
+train1['weekday'][(train1['weekday'] == 4)] = 1
+train1['weekday'][(train1['weekday'] == 5)] = 2
+train1['weekday'][(train1['weekday'] == 6)] = 3
+
+train1['timelabel'] = 0
+
+train1['timelabel'][((train1['time'] > datetime.time(0, 0, 0)) & (train1['time'] <= datetime.time(6, 0, 0)))] = int(0)
+train1['timelabel'][((train1['time'] > datetime.time(6, 0, 0)) & (train1['time'] <= datetime.time(12, 0, 0)))] = int(1)
+train1['timelabel'][((train1['time'] > datetime.time(12, 0, 0)) & (train1['time'] <= datetime.time(18, 0, 0)))] = int(2)
+train1['timelabel'][((train1['time'] > datetime.time(18, 0, 0)) & (train1['time'] <= datetime.time(23, 59, 59)))] = int(3)
+
+result = train1.drop('time', axis=1)
+result = result.drop('starttime', axis=1)
+print result
+
+# & (str(train1['time']) <= '23:59:59')]
+
+# train1['time'][((str(train1['time']) > '00:00:00') & (str(train1['time']) <= '07:00:00'))] = 0
+# train1['time'][(str(train1['time']) > '07:00:00' & str(train1['time']) <= '13:00:00')] = 1
+# train1['time'][((str(train1['time']) > '13:00:00') & (str(train1['time']) <= '18:00:00'))] = 2
+# train1['time'][(str(train1['time']) > '18:00:00' & str(train1['time']) <= '23:59:59')] = 3
+# train1['time'][(train1['time'] > pd.to_datetime('07:00:00').time()) & (train1['time'] <= pd.to_datetime('13:00:00').time())] = 1
+# train1['time'][(train1['time'] > pd.to_datetime('13:00:00').time()) & (train1['time'] <= pd.to_datetime('18:00:00').time())] = 2
+# train1['time'][(train1['time'] > pd.to_datetime('18:00:00').time()) & (train1['time'] <= pd.to_datetime('23:59:59').time())] = 3
+# print train1['time'].values
+
+# print train1
+
